b793a18700bc42c766543f5139c19469d2ccc7f2
Prnzdiamond <oseahumenagboifoh@gmail.com>
2024-02-18 21:50:48 +0100

    finished first part type of upcoming events

--- index.php
+++ index.php
@@ -9,6 +9,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document <?php echo $site; ?></title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" integrity="sha512-tS3S5qG0BlhnQROyJXvNjeEM4UpMXHrQfTGmbQ1gKmelCxlSEBUaxhRBj/EFTzpbP4RVSrpEikbmdJobCvhE3g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css" integrity="sha512-sMXtMNL1zRzolHYKEujM2AqCLUR9F2C4/05cdbxjjLSRvMQIciEPCQZo++nk7go3BtSuK9kfa/s+a4f4i5pLkw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    
 
     <?php include 'inc/header.layout.php'; ?>
 </head>
@@ -16,9 +19,230 @@
     <header class="header_four">
     <?php include 'inc/nav.layout.php'; ?>
     </header>
-        
-    
+    <header class="header_3">
+        <div class="intro_wrapper">
+		<div class="container">  
+			<div class="row">        
+				 <div class="col-sm-12 col-md-8 col-lg-8">
+					<div class="intro_text">
+						<h1>Take The World's Best Courses Online.</h1>
+						<p>Ante amet vitae vulputate odio nulla vel pretium pulvinar aenean.<br> Poncus eget adipiscing etiam arcu Ultricies.</p>
+						<div class="intro_button">
+							<a href="#" class="nav-link">Start my free month</a>
+						</div>
+					</div>
+				</div>		
+			</div>
+		</div> 
+	</div>  
+</header>  
+<!-- after landing page -->
+<section class="cources_highlight">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-sm-12 col-md-12 col-lg-12">
+                <div class="latest_blog_carousel">
+                     <div class="single_item single_item_first">
+                        <div class="blog-img">
+                            <a href="#"><img src="two/images/features/features_1.jpg" alt="" class="img-fluid"></a>
+                        </div>
+                        <div class="blog_title">
+                            <span>look At</span>  
+                            <h3><a href="#" title="">Next Scheduled</a></h3> 
+                            <p>Lorem Ipsum dolor sit amet mollis felis arcu donec viverra. Pede phasellus etiam; Aaecenas vel vici quis dictum rutrum nec nisi et ac penatibus aenean....</p>                    
+                        </div>   
+                    </div>
+
+                    <div class="single_item single_item_center">
+                        <div class="blog-img">
+                            <a href="#" title=""><img src="two/images/features/features_2.jpg" alt="" class="img-fluid"></a>
+                        </div>
+                        <div class="blog_title">
+                            <span>Go To</span>  
+                            <h3><a href="#" title="">Online Courees</a></h3> 
+                            <p>Lorem Ipsum dolor sit amet mollis felis arcu donec viverra. Pede phasellus etiam; Aaecenas vel vici quis dictum rutrum nec nisi et ac penatibus aenean. Pede enim nunc ultricies quis rhoncus penatibus tincidunt integer felis...</p>                    
+                        </div>   
+                    </div>
+
+                   <div class="single_item single_item_last">
+                        <div class="blog-img">
+                            <a href="#" title=""><img src="two/images/features/features_3.jpg" alt="" class="img-fluid"></a>
+                        </div>
+                        <div class="blog_title">
+                            <span>Get Your</span>  
+                            <h3><a href="#" title="">Education Equip</a></h3> 
+                             <p>Lorem Ipsum dolor sit amet mollis felis arcu donec viverra. Pede phasellus etiam; Aaecenas vel vici quis dictum rutrum nec nisi et ac penatibus aenean....</p>  
+                        </div>   
+                    </div>
+
+                </div>
+            </div>             
+        </div>
+    </div>
+</section>
+<!-- UPCOMING EVENTS -->
+ <section class="ttm-row event-section clearfix">
+            <div class="container">
+                <!-- row -->
+                <div class="row">
+                    <div class="col-md-2"></div>
+                    <div class="col-md-8">
+                        <!-- section title -->
+                        <div class="section-title with-desc title-style-center_text clearfix">
+                            <div class="title-header">
+                                <h5>OUR EVENTS</h5>
+                                <h2 class="title">Our Upcoming Events</h2>
+                            </div>
+                            <div class="title-desc">Raising a heavy fur muff that covered the whole of her lower arm towards the viewer regor then turned to look out the window</div>
+                        </div><!-- section title end -->
+                    </div>
+                    <div class="col-md-2"></div>
+                </div><!-- row end-->
+                <div class="row">
+                    <div class="wrap-event event-slide owl-carousel" data-item="3" data-nav="false" data-dots="false" data-auto="false">
+                        <!-- featured-imagebox-team -->
+                        <div class="featured-imagebox featured-imagebox-event mb-20">
+                            <div class="featured-thumbnail">
+                                <div class="ttm-box-post-date">
+                                    <span class="ttm-entry-date">
+                                        <time class="entry-date" datetime="2019-01-16T07:07:55+00:00">15<span class="entry-month entry-year">Mar</span></time>
+                                    </span>
+                                </div>
+                                <img class="img-fluid" src="assets/images/events/upcomingtes2.jpg" alt="image"> 
+                            </div> 
+                            <div class="featured-content featured-content-event box-shadow2">
+                                <div class="ttm-box-meta">
+                                    <span class="ttm-event-meta-item ttm-event-date"> 
+                                        <i class="fas fa-clock-o"></i> 
+                                        <span class="ttm-event-meta-dtstart"> November 28 </span>
+                                        <span class="sep">-</span> 
+                                        <span class="ttm-event-meta-dtend">8:00 am - 5:00 pm </span>
+                                    </span>
+                                    <div class="tribe-events-vanue"> 
+                                        <i class="fas fa-map-marker"></i>26 New York City  
+                                    </div>
+                                </div>
+                                <div class="featured-title pt-10">
+                                    <h5><a href="event-details.html">Seminar of Different Types of Plumbing Control in 2019</a></h5>
+                                </div>
+                                <a class="ttm-btn ttm-btn-size-sm ttm-icon-btn-right ttm-btn-color-black btn-inline" href="#">VIEW DETAILS<i class="ti ti-angle-double-right"></i></a>
+                            </div>
+                        </div><!-- featured-imagebox-team end-->
+                        <!-- featured-imagebox-team -->
+                        <div class="featured-imagebox featured-imagebox-event mb-20">
+                            <div class="featured-thumbnail">
+                                <div class="ttm-box-post-date">
+                                    <span class="ttm-entry-date">
+                                        <time class="entry-date" datetime="2019-01-16T07:07:55+00:00">16<span class="entry-month entry-year">Feb</span></time>
+                                    </span>
+                                </div>
+                                <img class="img-fluid" src="assets/images/events/upcomingtes2.jpg" alt="image"> 
+                            </div> 
+                            <div class="featured-content featured-content-event box-shadow2">
+                                <div class="ttm-box-meta">
+                                    <span class="ttm-event-meta-item ttm-event-date"> 
+                                        <i class="fas fa-clock-o"></i> 
+                                        <span class="ttm-event-meta-dtstart"> November 28 </span>
+                                        <span class="sep">-</span> 
+                                        <span class="ttm-event-meta-dtend">8:00 am - 5:00 pm </span>
+                                    </span>
+                                    <div class="tribe-events-vanue"> 
+                                        <i class="fas fa-map-marker"></i>26 New York City  
+                                    </div>
+                                </div>
+                                <div class="featured-title pt-10">
+                                    <h5><a href="event-details.html">Clean Air Day Awareness of Indoor Air Pollution</a></h5>
+                                </div>
+                                <a class="ttm-btn ttm-btn-size-sm ttm-icon-btn-right ttm-btn-color-black btn-inline" href="#">VIEW DETAILS<i class="ti ti-angle-double-right"></i></a>
+                            </div>
+                        </div><!-- featured-imagebox-team end-->
+                        <!-- featured-imagebox-team -->
+                        <div class="featured-imagebox featured-imagebox-event mb-20">
+                            <div class="featured-thumbnail">
+                                <div class="ttm-box-post-date">
+                                    <span class="ttm-entry-date">
+                                        <time class="entry-date" datetime="2019-01-16T07:07:55+00:00">20<span class="entry-month entry-year">Mar</span></time>
+                                    </span>
+                                </div>
+                                <img class="img-fluid" src="assets/images/events/upcomingtes2.jpg" alt="image"> 
+                            </div> 
+                            <div class="featured-content featured-content-event box-shadow2">
+                                <div class="ttm-box-meta">
+                                    <span class="ttm-event-meta-item ttm-event-date"> 
+                                        <i class="fas fa-clock-o"></i> 
+                                        <span class="ttm-event-meta-dtstart"> November 28 </span>
+                                        <span class="sep">-</span> 
+                                        <span class="ttm-event-meta-dtend">8:00 am - 5:00 pm </span>
+                                    </span>
+                                    <div class="tribe-events-vanue"> 
+                                        <i class="fas fa-map-marker"></i>26 New York City  
+                                    </div>
+                                </div>
+                                <div class="featured-title pt-10">
+                                    <h5><a href="event-details.html">Camps for New Inventions of Faucets Cleaning</a></h5>
+                                </div>
+                                <a class="ttm-btn ttm-btn-size-sm ttm-icon-btn-right ttm-btn-color-black btn-inline" href="#">VIEW DETAILS<i class="ti ti-angle-double-right"></i></a>
+                            </div>
+                        </div><!-- featured-imagebox-team end-->
+                        <!-- featured-imagebox-team -->
+                        <div class="featured-imagebox featured-imagebox-event mb-20">
+                            <div class="featured-thumbnail">
+                                <div class="ttm-box-post-date">
+                                    <span class="ttm-entry-date">
+                                        <time class="entry-date" datetime="2019-01-16T07:07:55+00:00">27<span class="entry-month entry-year">Dec</span></time>
+                                    </span>
+                                </div>
+                                <img class="img-fluid" src="assets/images/events/upcomingtes2.jpg" alt="image"> 
+                            </div> 
+                            <div class="featured-content featured-content-event box-shadow2">
+                                <div class="ttm-box-meta">
+                                    <span class="ttm-event-meta-item ttm-event-date"> 
+                                        <i class="fas fa-clock-o"></i> 
+                                        <span class="ttm-event-meta-dtstart"> November 28 </span>
+                                        <span class="sep">-</span> 
+                                        <span class="ttm-event-meta-dtend">8:00 am - 5:00 pm </span>
+                                    </span>
+                                    <div class="tribe-events-vanue"> 
+                                        <i class="fas fa-map-marker"></i>26 New York City  
+                                    </div>
+                                </div>
+                                <div class="featured-title pt-10">
+                                    <h5><a href="event-details.html">Tools Camp For Handymans in Newyork City</a></h5>
+                                </div>
+                                <a class="ttm-btn ttm-btn-size-sm ttm-icon-btn-right ttm-btn-color-black btn-inline" href="#">VIEW DETAILS<i class="ti ti-angle-double-right"></i></a>
+                            </div>
+                        </div><!-- featured-imagebox-team end-->
+                    </div>
+                </div>
+            </div>
+        </section>
     <?php include 'inc/footer.layout.php'; ?>
     <?php include 'inc/scripts.layout.php'; ?>
+
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js" integrity="sha512-bPs7Ae6pVvhOSiIcyUClR7/q2OAsRiovw4vAkX+zJbw3ShAeeqezq50RIIcIURq7Oa20rW2n2q+fyXBNcU9lrw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script type="text/javascript">
+        //var count = $('.owl-carousel').getAttr('data-item');
+        $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    smartSpeed:3000,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
+});
+    </script>
 </body>
 </html>
